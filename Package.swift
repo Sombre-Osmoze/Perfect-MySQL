d3ed7f10915686330ac479d8b4caac0a0485b548
Marcus Joachim Florentin <fmarcus28@gmx.fr>
2018-07-10 13:51:22 +0200

    Choosing  master branch

--- Package.swift
+++ Package.swift
@@ -32,7 +32,7 @@ let package = Package(
 	],
 	dependencies: [
 		.package(url: "https://github.com/PerfectlySoft/Perfect-CRUD.git", from: "1.0.0"),
-		.package(url: "https://github.com/Sombre-Osmoze/\(clientPackage).git", majorVersion: 2),
+		.package(url: "https://github.com/Sombre-Osmoze/\(clientPackage).git", branch: "master"),
 		],
 	targets: [
 		.target(name: "PerfectMySQL", dependencies: ["PerfectCRUD"]),
