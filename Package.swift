170d3ffb7b3f4d47c275d62d0e812b686eb61cdf
Marcus Joachim Florentin <fmarcus28@gmx.fr>
2018-07-10 14:07:56 +0200

    3.0.0

--- Package.swift
+++ Package.swift
@@ -32,7 +32,7 @@ let package = Package(
 	],
 	dependencies: [
 		.package(url: "https://github.com/PerfectlySoft/Perfect-CRUD.git", from: "1.0.0"),
-		.package(url: "https://github.com/Sombre-Osmoze/\(clientPackage).git", from: "2.1.0"),
+		.package(url: "https://github.com/Sombre-Osmoze/\(clientPackage).git", from: "3.0.0"),
 		],
 	targets: [
 		.target(name: "PerfectMySQL", dependencies: ["PerfectCRUD"]),
